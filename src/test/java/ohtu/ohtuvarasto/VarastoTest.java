dfe017e8f5f15dcd1d0f7394654d2b2fa7072925
klaufred <klaus.fredriksson@helsinki.fi>
2018-11-04 20:05:53 +0200

    travis badge

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -80,7 +80,7 @@ public class VarastoTest {
     public void lisaysLisaaSaldoa() {
         varasto.lisaaVarastoon(8);
 
-        // saldon pitäisi olla sama kun lisätty määrä
+        // saldon pitäisi olla sama kun lisätty määrä.
         assertEquals(8, varasto.getSaldo(), vertailuTarkkuus);
     }
 
